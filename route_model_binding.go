e842640e7bcd2798b015698a2cc95d42b4f3e3e2
Seth Phat <phattranminh96@gmail.com>
2022-05-14 16:57:39 +0700

    Create route_model_binding.go

---
+++ route_model_binding.go
@@ -0,0 +1,68 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"pheasant-api/app/models"
+)
+
+type modelMapping struct {
+	model  interface{}
+	column string
+}
+
+var routeModelMapping = map[string]modelMapping{
+	"entity": makeModelMapping(&models.Entity{}, "uuid"),
+}
+
+func makeModelMapping(model interface{}, column string) modelMapping {
+	return modelMapping{
+		model:  model,
+		column: column,
+	}
+}
+
+func RouteModelBinding() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		routeParamValue := ""
+		routeParamKey := ""
+		var routeParamModel modelMapping
+
+		for routeParam, model := range routeModelMapping {
+			routeParamModel = model
+			routeParamKey = routeParam
+			routeParamValue = c.Param(routeParam)
+
+			if routeParamValue != "" {
+				break
+			}
+		}
+
+		if routeParamValue != "" {
+			retrieveModel(c, routeParamModel, routeParamValue, routeParamKey)
+		} else {
+			c.Next()
+		}
+	}
+}
+
+func retrieveModel(c *gin.Context, modelMapInfo modelMapping, routeValue string, routeParamKey string) {
+	model := modelMapInfo.model
+	queryResult := models.DB.Where(modelMapInfo.column+" = ?", routeValue).First(model)
+	if queryResult.Error != nil {
+		log.Print(queryResult.Error)
+		abortNotFound(c)
+
+		return
+	}
+
+	c.Set(routeParamKey, model)
+	c.Next()
+}
+
+func abortNotFound(c *gin.Context) {
+	c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+		"message": "Resource not found",
+	})
+}
